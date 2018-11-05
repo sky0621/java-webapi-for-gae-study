a4dd386a5c19d4bfd9d9c953e2c4d8d076eba556
sky0621 <sky14work@gmail.com>
2018-11-05 23:46:13 +0900

    skelton endpoint

---
+++ com.example.sky0621.fs/src/main/java/com/example/sky0621/fs/YourFirstAPI.java
@@ -0,0 +1,12 @@
+package com.example.sky0621.fs;
+
+import com.google.api.server.spi.config.Api;
+
+/**
+  * Add your first API methods in this class, or you may create another class. In that case, please
+  * update your web.xml accordingly.
+ **/
+@Api(name = "skeleton-api",
+     version = "v1")
+public class YourFirstAPI {
+}
