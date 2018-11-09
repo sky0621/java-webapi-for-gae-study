c2e300eaa7e79cdaf396fdf468e102a8d235507d
sky0621 <sky14work@gmail.com>
2018-11-10 02:07:35 +0900

    temp

---
+++ com.example.sky0621.fs/src/main/java/com/example/sky0621/fs/builder/Builder.java
@@ -0,0 +1,9 @@
+package com.example.sky0621.fs.builder;
+
+import com.example.sky0621.fs.other.Data1;
+
+@lombok.Builder
+public class Builder {
+    private int no;
+    private Data1 data1;
+}
---
+++ com.example.sky0621.fs/src/main/java/com/example/sky0621/fs/other/Data1.java
@@ -0,0 +1,9 @@
+package com.example.sky0621.fs.other;
+
+import lombok.Data;
+
+@Data
+public class Data1 {
+    private int id;
+    private String text;
+}
